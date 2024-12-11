654ef82bf3ea103049c3feb4429560adf11e3d92
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2024-12-11 23:01:00 +0000

    migrated code bulk into the functions to clean up GET

--- src/api/data.ts
+++ src/api/data.ts
@@ -7,24 +7,46 @@ import { get } from "http";
 const DATA_URL =
     "https://sampleapi.squaredup.com/integrations/v1/service-desk?datapoints=500";
 
-const calculatePercentageTypes = (data: SampleData, type: string) => {
+const calculatePercentageTypes = (data: SampleData) => {
     const results = data.results;
 
-    const count = results.filter((issue) => issue.type === type).length;
+    const issueTypes = ["question", "problem", "task"];
 
-    const percentage = count / results.length;
+    const issueCounts: { type: string; percentage: number }[] = [];
 
-    return percentage * 100;
+    issueTypes.forEach((issueType) => {
+        const count = results.filter(
+            (issue) => issue.type === issueType
+        ).length;
+        const percentage = (count / results.length) * 100;
+        issueCounts.push({
+            type: issueType,
+            percentage: percentage,
+        });
+    });
+
+    return issueCounts;
 };
 
-const calculatePercentagePriority = (data: SampleData, priority: string) => {
+const calculatePercentagePriority = (data: SampleData) => {
     const results = data.results;
 
-    const count = results.filter((issue) => issue.priority === priority).length;
+    const priorityTypes = ["low", "normal", "high"];
 
-    const percentage = count / results.length;
+    const priorityCounts: { type: string; percentage: number }[] = [];
 
-    return percentage * 100;
+    priorityTypes.forEach((priorityType) => {
+        const count = results.filter(
+            (issue) => issue.priority === priorityType
+        ).length;
+        const percentage = (count / results.length) * 100;
+        priorityCounts.push({
+            type: priorityType,
+            percentage: percentage,
+        });
+    });
+
+    return priorityCounts;
 };
 
 const calculateCloseTime = (issue: { created: string; updated: string }) => {
@@ -35,6 +57,25 @@ const calculateCloseTime = (issue: { created: string; updated: string }) => {
         (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60);
 
     return average;
+};
+
+const calculateAverageTime = (data: SampleData) => {
+    const results = data.results;
+
+    var timeTotal = 0;
+    const count = results.filter((issue) => issue.priority === "high").length;
+
+    results.forEach((issue) => {
+        const average = calculateCloseTime(issue);
+
+        if (issue.priority === "high") {
+            timeTotal += average;
+        }
+    });
+
+    const averageTime = timeTotal / count;
+
+    return averageTime;
 };
 
 const getScoreValue = (data: SampleData, averageTime: number) => {
@@ -57,71 +98,5 @@ export const GET = async (req: Request, res: Response) => {
 
     const results = data.results;
 
-    const issueTypes = ["question", "problem", "task"];
-
-    const issueCounts: { type: string; percentage: number }[] = [];
-
-    issueTypes.forEach((issueType) => {
-        const percentage = calculatePercentageTypes(data, issueType);
-        issueCounts.push({
-            type: issueType,
-            percentage: percentage,
-        });
-
-        //res.status(200).json(calculatePercentage(data, issueType));
-    });
-
-    const priorityTypes = ["low", "normal", "high"];
-
-    const priorityCounts: { type: string; percentage: number }[] = [];
-
-    const averageCounts: { priority: string; average: number }[] = [];
-
-    priorityTypes.forEach((priorityType) => {
-        const percentage = calculatePercentagePriority(data, priorityType);
-        priorityCounts.push({
-            type: priorityType,
-            percentage: percentage,
-        });
-
-        //res.status(200).json(calculatePercentage(data, issueType));
-    });
-
-    var timeTotal = 0;
-    const count = results.filter((issue) => issue.priority === "high").length;
-
-    results.forEach((issue) => {
-        const average = calculateCloseTime(issue);
-
-        if (issue.priority === "high") {
-            averageCounts.push({ priority: issue.priority, average: average });
-            timeTotal += average;
-        }
-    });
-
-    const averageTime = timeTotal / count;
-
-    res.status(200).json(getScoreValue(data, averageTime));
-
-    //res.status(200).json(averageCounts);
-
-    //res.status(200).json(timeTotal / count);
-
-    //res.status(200).json(priorityCounts);
-
-    // const averageCounts: { priority: string; average: number }[] = [];
-
-    // results.forEach((issue) => {
-    //     const startDate = new Date(issue.created);
-    //     const endDate = new Date(issue.updated);
-
-    //     const average =
-    //         (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60);
-
-    //     averageCounts.push({ priority: issue.priority, average: average });
-    // });
-
-    //res.status(200).json(averageCounts);
-
-    //res.send(data);
+    res.status(200).json(calculateAverageTime(data));
 };
